996317ce640108e5194d777008c438477ded3e66
patrik7776 <142219712+patrik7776@users.noreply.github.com>
2023-09-21 10:52:30 +0200

    Add files via upload
    dune

---
+++ js.js
@@ -0,0 +1,15 @@
+/*var triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'))
+triggerTabList.forEach(function (triggerEl) {
+  var tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', function (event) {
+    event.show()
+    tabTrigger.show()
+    console.log(triggerEl)
+  })
+})*/
+
+/*var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+  return new bootstrap.Dropdown(dropdownToggleEl)
+})*/
